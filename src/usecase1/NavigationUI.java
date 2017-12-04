e7110cea4fdc627e7d252b27d6767947935a4225
jacob <jacob@192.168.1.19>
2017-12-03 20:33:45 -0500

    Bugfix for navigation launch

--- src/usecase1/NavigationUI.java
+++ src/usecase1/NavigationUI.java
@@ -7,45 +7,46 @@ package usecase1;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
  
 public class NavigationUI {
-    @FXML private Text jobSearch;
-    @FXML private Text actiontarget;
-    @FXML private Text jobPost;
-    @FXML private Text personalityTest;
-    @FXML private Text Exit;
+    @FXML private Button jobSearch;
+    @FXML private Button actiontarget;
+    @FXML private Button jobPost;
+    @FXML private Button personalityTest;
+    @FXML private Button Exit;
      
     
     @FXML protected void handleExitButtonAction(ActionEvent event) {
-        Exit.setText("Exit button pressed");
+        //Exit.setText("Exit button pressed");
         Stage theStage = (Stage) Exit.getScene().getWindow();
         NavigationController.getNavigationController(theStage).exit();
     }
     
     @FXML protected void handleUsersButtonAction(ActionEvent event) {
-        actiontarget.setText("Profile button pressed");
+        //actiontarget.setText("Profile button pressed");
         Stage theStage = (Stage) actiontarget.getScene().getWindow();
         theStage.hide();
         NavigationController.getNavigationController(theStage).getProfileController(theStage);
     }
     @FXML protected void handleUC2ButtonAction(ActionEvent event) {
-        personalityTest.setText("Personality Test button pressed");
+        //personalityTest.setText("Personality Test button pressed");
         Stage theStage = (Stage) personalityTest.getScene().getWindow();
         theStage.hide();
         NavigationController.getNavigationController(theStage).getTestController(theStage);
     }
     
     @FXML protected void handleUC3ButtonAction(ActionEvent event) {
-        jobPost.setText("Post Job button pressed");
+        //jobPost.setText("Post Job button pressed");
         Stage theStage = (Stage) jobPost.getScene().getWindow();
         theStage.hide();
         NavigationController.getNavigationController(theStage).getPostJobController(theStage);
     }
     
         @FXML protected void handleUC4ButtonAction(ActionEvent event) {
-        jobSearch.setText("Job Seach button pressed");
+        //jobSearch.setText("Job Seach button pressed");
         Stage theStage = (Stage) jobSearch.getScene().getWindow();
         theStage.hide();
         NavigationController.getNavigationController(theStage).getJobSearchController(theStage);
